ec8751db9803399ca7f965406361c75f41914a88
Victor Pudo <cpt.victor@hotmail.com>
2023-12-08 18:13:39 -0300

    fix: remove linting errors

--- src/components/auth-button.tsx
+++ src/components/auth-button.tsx
@@ -1,19 +1,20 @@
-"use client"
+"use client";
 
-import { useRouter } from 'next/navigation';
-import React from 'react'
-import { Button } from './ui/button';
-import { Session } from 'next-auth';
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+import { type Session } from "next-auth";
 
 export const AuthButton = async ({ session }: { session: Session | null }) => {
-
-  const { push } = useRouter();
+  const router = useRouter();
 
   return (
-    
-            <Button onClick={() => push(session ? "/api/auth/signout" : "/api/auth/signin")}>
-              {session ? "Sign out" : "Sign in"}
-            </Button>
-          
-  )
-}
+    <Button
+      onClick={() =>
+        router.push(session ? "/api/auth/signout" : "/api/auth/signin")
+      }
+    >
+      {session ? "Sign out" : "Sign in"}
+    </Button>
+  );
+};
